9846d1deb1f7151a8c8dc58dcb78ec5904bdd8bd
Nourn117 <noranelsherbeny2@gmail.com>
2025-07-20 15:44:53 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -103,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
+       
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
@@ -116,10 +117,15 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
   ////
 ////sssss
 /////////////ssssss
+///ظظظظظ
+///ىnono
+
+
 }
